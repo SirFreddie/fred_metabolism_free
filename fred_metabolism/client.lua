9377f124fb1e156f9bcf536590c02da3818f8361
Sir Freddie <67666108+SirFreddie@users.noreply.github.com>
2021-01-22 17:40:04 -0300

    Update client.lua

--- fred_metabolism/client.lua
+++ fred_metabolism/client.lua
@@ -132,3 +132,66 @@ AddEventHandler("fred:useItem", function(index)
 
 	TaskItemInteraction(PlayerPedId(), nil, GetHashKey("EAT_MULTI_BITE_FOOD_SPHERE_D8-2_SANDWICH_QUICK_LEFT_HAND"), true, 0, 0)
 end)
+
+-- EVENT, USE THIS FOR OTHER RESOURCES
+RegisterNetEvent("fred:consume")
+AddEventHandler("fred:consume", function(hunger,thirst, innercorestamina, innercorestaminagold, outercorestaminagold, innercorehealth, innercorehealthgold, outercorehealthgold)
+	local ped = PlayerPedId()
+	local coords = GetEntityCoords(ped)
+	food = food + tonumber(hunger)
+	water = water + tonumber(thirst)
+
+	if (food < 0) then
+		food = 0
+	end
+
+	if (food > 100) then
+		food = 100
+	end
+
+	if water < 0 then
+		water = 0
+	end
+
+	if water > 100 then
+		water = 100
+	end
+
+
+	if (innercorestamina ~= 0) then
+		stamina = Citizen.InvokeNative(0x36731AC041289BB1, ped, 1) --ACTUAL STAMINA CORE GETTER
+		newStamina = stamina + tonumber(innercorestamina)
+
+		if (newStamina > 100) then
+		newStamina = 100
+		end
+		Citizen.InvokeNative(0xC6258F41D86676E0, ped, 1, newStamina)
+	end
+
+	if (innercorehealth ~= 0)then
+		health = Citizen.InvokeNative(0x36731AC041289BB1, ped, 0) --ACTUAL HEALTH CORE GETTER
+		newHealth = health + tonumber(innercorehealth)
+
+		if (newHealth > 100) then
+		newHealth = 100
+		end
+		Citizen.InvokeNative(0xC6258F41D86676E0, ped, 0, newHealth)
+	end
+
+		--TO DO OUTER CORE HEALTH parametro = outerCoreHealth
+
+	--GOLDS
+
+	if (innercorestaminagold ~= 0.0) then
+		Citizen.InvokeNative(0x4AF5A4C7B9157D14, PlayerPedId(), 1, innercorestaminagold, true)
+	end
+	if (outercorestaminagold ~= 0.0) then
+		Citizen.InvokeNative(0xF6A7C08DF2E28B28, PlayerPedId(), 1, outercorestaminagold, true)
+	end
+	if (innercorehealthgold ~= 0.0) then
+		Citizen.InvokeNative(0x4AF5A4C7B9157D14, PlayerPedId(), 0, innercorehealthgold, true)
+	end
+	if	(outercorehealthgold ~= 0.0) then
+		Citizen.InvokeNative(0xF6A7C08DF2E28B28, PlayerPedId(), 0, outercorehealthgold, true)
+	end
+end)
